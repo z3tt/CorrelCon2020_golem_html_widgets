426b0a211a5289db05115ee3886c995b81784376
Z3tt <cedricphilippscherer@gmail.com>
2020-11-06 10:21:13 +0100

    :art: add buttons echart

--- R/mod_bars_echarts.R
+++ R/mod_bars_echarts.R
@@ -13,6 +13,39 @@ mod_bars_echarts_ui <- function(id){
   fullPage::pageContainer(
     h1("An interactive bar chart"),
     br(),
+    ##### ADD BUTTIONS FOR SOURCE DATA + CHART TYPE ----------------------------
+    fluidRow(
+      column(
+        4,
+        shinyWidgets::radioGroupButtons(
+          inputId = ns("data"),
+          label = "Choose data",
+          choices = c("All accidents", "Bike accidents"),
+          checkIcon = list(
+            yes = icon("ok",
+                       lib = "glyphicon")
+          )
+        )
+      ),
+      column(
+        4,
+        uiOutput(ns("types_select_generated"))
+      ),
+      column(
+        4,
+        shinyWidgets::radioGroupButtons(
+          inputId = ns("chart"),
+          label = "Choose chart type",
+          choices = c("Dodged bars", "Stacked bars"),
+          selected = "Stacked bars",
+          checkIcon = list(
+            yes = icon("ok",
+                       lib = "glyphicon")
+          )
+        )
+      )
+    ),
+    ##### END ADD BUTTIONS FOR SOURCE DATA + CHART TYPE ------------------------
     shinycssloaders::withSpinner(
       echarts4r::echarts4rOutput(ns("bars"), height = "50vh")
     ),
@@ -28,6 +61,19 @@ mod_bars_echarts_ui <- function(id){
 mod_bars_echarts_server <- function(input, output, session){
   ns <- session$ns
   
+  ##### ADD SELECTED OUTPUT ----------------------------------------------------
+  output$types_select_generated <- renderUI({
+    
+    selectizeInput(
+      ns("types_select"),
+      "Choose road user",
+      choices = c("Bikes", "Cars", "Motorcycles", "Pedestrians"),
+      selected = c("Bikes", "Cars"),
+      multiple = TRUE
+    )
+  })
+  ##### END ADD SELECTED OUTPUT ------------------------------------------------
+  
   output$bars <- echarts4r::renderEcharts4r({
     # set typeface for echarts
     echarts4r::e_common(
@@ -41,13 +87,17 @@ mod_bars_echarts_server <- function(input, output, session){
       color = c("#b03a47", "#293c55", "#6ab0b8", "#e98f6f")
     )
     
-    dat <- correlcon::accidents_sum_bikes %>%
-      dplyr::left_join(my_colors, by = "type")
+    ##### ADD VARIOUS DATA SOURCES ---------------------------------------------
+    if (input$data == "All accidents") { dat <- xberlin::accidents_sum_all }
+    if (input$data == "Bike accidents") { dat <- xberlin::accidents_sum_bikes }
     
-    dat %>%
+    dat <- dplyr::left_join(dat, my_colors, by = "type")
+    ##### END ADD VARIOUS DATA SOURCES -----------------------------------------
+    
+     e <- dat %>%
       dplyr::group_by(type) %>% 
       echarts4r::e_charts(Gemeinde_name) %>% 
-      echarts4r::e_bar(accidents) %>% 
+      #echarts4r::e_bar(accidents) %>% 
       echarts4r::e_x_axis(axisTick = list(interval = 0), 
                           axisLabel = list(rotate = 30), 
                           nameGap = 35) %>% 
@@ -56,6 +106,14 @@ mod_bars_echarts_server <- function(input, output, session){
       echarts4r::e_toolbox_feature(feature = "dataZoom") %>%
       echarts4r::e_toolbox_feature(feature = "dataView") %>%
       echarts4r::e_show_loading()
+    
+    ##### CHANGE CHART TYPE BASED ON INPUT -------------------------------------
+    if (input$chart == "Dodged bars") {
+      e %>% echarts4r::e_bar(accidents)
+    } else {
+      e %>% echarts4r::e_bar(accidents, stack = "grp")
+    }
+    ##### END CHANGE CHART TYPE BASED ON INPUT ---------------------------------
   })
 }
     
